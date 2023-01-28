b7830e752d142e66f3be0337e22794e60cba7546
Josh Pooley <joshpooley93@gmail.com>
2023-01-28 21:16:14 +0000

    update skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -13,7 +13,7 @@ export const skills = [ // List of contact information
     },
     {
         name: "Front-End Development",
-        text: "HTML5, CSS/SCSS, Vanilla JS, Leaflet, Svelte, Node",
+        text: "HTML, CSS/SCSS, JavaScript/TypeScript, Leaflet, Svelte, Node",
         rating: "75"
     },
     {
