0d1e8bd91d8b88d5be0ddd3c368ccb9e69ee3e4a
Josh Pooley <joshpooley93@gmail.com>
2023-06-01 23:05:22 +0100

    feat: added interleaving masonry columns

---
+++ src/lib/components/Columns.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	let width: number;
+
+	const mobileBreak = 600;
+</script>
+
+<main>
+	{#if width > mobileBreak}
+		<div class="left">
+			<slot name="left" />
+		</div>
+
+		<div class="right">
+			<slot name="right" />
+		</div>
+	{:else}
+		<div class="center">
+			<slot name="left" />
+			<slot name="right" />
+		</div>
+	{/if}
+</main>
+
+<svelte:window bind:innerWidth={width} />
+
+<style lang="scss">
+	main {
+		width: 100%;
+		max-width: 80rem;
+		background: $base-color;
+
+		display: flex;
+	}
+
+	div {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.left {
+		width: 40%;
+	}
+
+	.right {
+		width: 60%;
+	}
+
+	.center {
+		width: 100%;
+	}
+</style>
